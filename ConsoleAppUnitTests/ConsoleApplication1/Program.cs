ea69eaab012362ca2a55c1fcca64d6786f7a9dc6
peshkovat@ukr.net <Lzlz:jhf1203>
2018-07-10 00:35:44 +0300

    ConsoleAppUnitTests
    ConsoleC#AppNUnitTests

---
+++ ConsoleAppUnitTests/ConsoleApplication1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAcount acc = new BankAcount(3333m);
+            Console.WriteLine(acc.Amount);
+        }
+    }
+}
